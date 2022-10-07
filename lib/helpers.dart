41701f2791f324ce2876df14664164bc470865f5
Lenz Paul <bringbackthedog@gmail.com>
2022-10-07 15:50:45 -0700

    rm dotEnv and moved functions from main

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -1,14 +1,15 @@
 // Copyright (c) 2021, the Dart project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
+// ignore_for_file: no_leading_underscores_for_local_identifiers
 
 import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
-
 import 'package:http/http.dart' as http;
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
-import 'package:dotenv/dotenv.dart';
 
 /// Serves [handler] on [InternetAddress.anyIPv4] using the port returned by
 /// [listenPort].
@@ -89,16 +90,10 @@ Future<void> terminateRequestFuture() {
 /// [Project metadata](https://cloud.google.com/compute/docs/metadata/default-metadata-values#project_metadata)
 /// is queried for the Project ID.
 Future<String> currentProjectId() async {
-  var env = DotEnv(includePlatformEnvironment: true)..load();
-
   for (var envKey in gcpProjectIdEnvironmentVariables) {
-    // final value = Platform.environment[envKey];
-    final value = env[envKey];
+    final value = Platform.environment[envKey];
     if (value != null) return value;
   }
-
-  // Check vscode environment variables
-
   const host = 'http://metadata.google.internal/';
   final url = Uri.parse('$host/computeMetadata/v1/project/project-id');
 
@@ -145,3 +140,155 @@ const gcpProjectIdEnvironmentVariables = {
   'CLOUDSDK_CORE_PROJECT',
   'GOOGLE_CLOUD_PROJECT',
 };
+
+class FirebaseAuthAPIMethods {
+  FirebaseAuthAPIMethods._();
+  static const signInWithPassword = 'signInWithPassword';
+  static const signInAnonymously = 'signUp';
+}
+
+class FirebaseAuthClient {
+  FirebaseAuthClient._();
+
+  static String get apiKey {
+    final env = Platform.environment;
+
+    if (env['FIREBASE_API_KEY'] == null) {
+      throw Exception(
+        'FIREBASE_API_KEY is not set. '
+        'Please set it in your .env file at the root of your project.\n'
+        'e.g.: FIREBASE_API_KEY="APIKEYHERE"',
+      );
+    }
+
+    return env['FIREBASE_API_KEY']!;
+  }
+
+  static const firebaseAuthBaseUrl =
+      'https://identitytoolkit.googleapis.com/v1/accounts';
+
+  static String get signInWithPasswordUrl =>
+      '$firebaseAuthBaseUrl:${FirebaseAuthAPIMethods.signInWithPassword}?key=$apiKey';
+
+  static String get signInAnonymouslyUrl =>
+      '$firebaseAuthBaseUrl:${FirebaseAuthAPIMethods.signInAnonymously}?key=$apiKey';
+
+  static loginAnonymouslyHandler(Request request) async {
+    log("Request: ${request.url}");
+
+    final http.Response result = await http.post(
+      Uri.parse(signInAnonymouslyUrl),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'returnSecureToken': true,
+      }),
+    );
+
+    Response response;
+
+    if (result.statusCode == 200) {
+      var body = jsonDecode(result.body);
+      var email = body['email']; // Empty string
+      var uid = body['localId'];
+
+      response = Response.ok(
+        JsonUtf8Encoder(' ').convert(
+          {
+            'email': email,
+            'uid': uid,
+          },
+        ),
+        headers: {
+          'content-type': 'application/json',
+        },
+      );
+    } else {
+      response = Response(
+        result.statusCode,
+        body: result.body,
+      );
+    }
+
+    log("Response: ${response.statusCode}");
+
+    return response;
+  }
+
+  /// Expects a [Request] with authorization header as follows:
+  /// {authorization: 'Basic <base64 encoded email:password>'}
+  ///
+  /// Returns a [Response] with the following body:
+  /// {
+  ///   "uid": "string",
+  ///   "email": "string"
+  /// }
+  static loginWithEmailAndPasswordHandler(Request request) async {
+    log("Request: ${request.url}");
+
+    var authorization = request.headers['authorization'];
+    if (authorization == null) {
+      return Response(401, body: 'Unauthorized. Missing authorization header.');
+    }
+
+    // Check authorization header for the type of authentication. Only support
+    // Firebase authentication for now with Basic Auth base64 encoded.
+    var authType = authorization.split(' ')[0];
+    if (authType != 'Basic') {
+      return Response(
+        401,
+        body: 'Invalid authorization type. '
+            'Only Basic is supported at this time.',
+      );
+    }
+
+    // If the authorization type is Basic, then decode the base64 encoded
+    // credentials and send to Firebase for authentication.
+    //
+    // The encoded credentials are expected to be in the format of
+    // <email>:<password>.
+    var authValue = authorization.split(' ')[1]; // base64 encoded credentials
+    var decoded = base64.decode(authValue); // decoded credentials in bytes
+    var decodedString = utf8.decode(decoded); // decoded credentials in string
+    var email = decodedString.split(':')[0];
+    var password = decodedString.split(':')[1];
+
+    final result = await http.post(
+      Uri.parse(signInWithPasswordUrl),
+      body: {
+        'email': email,
+        'password': password,
+      },
+    );
+
+    // Return the uid and the email of the firebase user.
+    var response;
+    if (result.statusCode == 200) {
+      var body = jsonDecode(result.body);
+      var email = body['email'];
+      var uid = body['localId'];
+
+      response = Response.ok(
+        JsonUtf8Encoder(' ').convert(
+          {
+            'email': email,
+            'uid': uid,
+          },
+        ),
+        headers: {
+          'content-type': 'application/json',
+        },
+      );
+    } else {
+      response = Response(
+        result.statusCode,
+        body: result.body,
+      );
+    }
+
+    log("Response: ${response.statusCode}");
+
+    return response;
+  }
+}
